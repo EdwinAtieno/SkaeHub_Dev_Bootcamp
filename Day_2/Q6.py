b59d43e8143d555df895164e0405dc6aa86873aa
Eddy <60142434+EdwinAtieno@users.noreply.github.com>
2021-06-23 23:51:48 +0300

    Add files via upload

---
+++ Day_2/Q6.py
@@ -0,0 +1,8 @@
+#get a list of value
+lst = [16, 2, 21, 17 , 19 , 4, 10, 5]
+# perfome the average using the sum() to get sum and len() to get the length of list
+avg=sum(lst) // len(lst)
+
+# print the sum after using sum()
+print("Sum= ", sum(lst))
+print("Average= ", +avg)
